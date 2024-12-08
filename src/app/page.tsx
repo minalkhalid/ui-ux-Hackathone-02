30b489fae7c9390668e7aca23f62f43694902e5a
minnal04 <minnalkhalid04@gmail.com>
2024-12-09 01:05:03 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+import HeroSection from "./hero/page";
+
+
+export default function Home(){
+  return(
+    <div >
+          <title>Hackathone e-commerce-project </title>
+          <HeroSection/>
+    </div>
+  )
+}
